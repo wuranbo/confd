edb422297988df75b912be815ac5fd59bbcf8aab
wuranbo <wuranbo@gmail.com>
2015-04-02 20:06:36 +0800

    *bugfix) 增加加减乘除。

--- resource/template/template_funcs.go
+++ resource/template/template_funcs.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -63,26 +64,144 @@ func ByteToM(data string) (string, error) {
 	}
 }
 
-func StringAdd(num1 int, num2 int) int {
-	return num1 + num2
-}
-
-func StringSub(num1 int, num2 int) int {
-	var rlt = num1 - num2
-	if rlt < 0 {
-		rlt = 0
+func toInt(data interface{}) (ret int64, err error) {
+	switch v := data.(type) {
+	case float32:
+		ret = int64(v)
+	case float64:
+		ret = int64(v)
+	case int:
+		ret = int64(v)
+	case int8:
+		ret = int64(v)
+	case int16:
+		ret = int64(v)
+	case int32:
+		ret = int64(v)
+	case uint:
+		ret = int64(v)
+	case uint8:
+		ret = int64(v)
+	case uint16:
+		ret = int64(v)
+	case uint32:
+		ret = int64(v)
+	case uint64:
+		ret = int64(v)
+	case string:
+		ret, err = strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			tmpfloat, terr := strconv.ParseFloat(v, 10)
+			ret = int64(tmpfloat)
+			err = terr // haha! go compile"s bug
+			if err != nil {
+				tmpuint, terr := strconv.ParseUint(v, 10, 63)
+				ret = int64(tmpuint)
+				err = terr
+			}
+		}
+	case []byte:
+		ret, err = strconv.ParseInt(string(v), 10, 64)
+		if err != nil {
+			tmpfloat, terr := strconv.ParseFloat(string(v), 10)
+			ret = int64(tmpfloat)
+			err = terr
+			if err != nil {
+				tmpuint, terr := strconv.ParseUint(string(v), 10, 63)
+				ret = int64(tmpuint)
+				err = terr
+			}
+		}
+	default:
+		ret = 0
 	}
-	return rlt
+	return ret, err
 }
 
-func StringMul(num1 int, num2 int) int {
-	return num1 * num2
-}
-
-func StringDiv(num1 int, num2 int) int {
-	var rlt = num1 / num2
-	if rlt <= 0 {
-		rlt = 1
+func StringAdd(num1, num2 interface{}) (ret string, reterr error) {
+	ret = "0"
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			reterr = r.(error)
+		}
+	}()
+	inta, reterr := toInt(num1)
+	if reterr != nil {
+		return
 	}
-	return rlt
+	intb, reterr := toInt(num2)
+	if reterr != nil {
+		return
+	}
+	ret = strconv.FormatInt(inta+intb, 10)
+	return
+}
+
+func StringSub(num1, num2 interface{}) (ret string, reterr error) {
+	ret = "0"
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			reterr = r.(error)
+		}
+	}()
+	inta, reterr := toInt(num1)
+	if reterr != nil {
+		return
+	}
+	intb, reterr := toInt(num2)
+	if reterr != nil {
+		return
+	}
+	ret = strconv.FormatInt(inta-intb, 10)
+	return
+}
+
+func StringMul(num1, num2 interface{}) (ret string, reterr error) {
+	ret = "0"
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			reterr = r.(error)
+		}
+	}()
+	inta, reterr := toInt(num1)
+	if reterr != nil {
+		return
+	}
+	intb, reterr := toInt(num2)
+	if reterr != nil {
+		return
+	}
+	ret = strconv.FormatInt(inta*intb, 10)
+	return
+}
+
+func StringDiv(num1, num2 interface{}) (ret string, reterr error) {
+	ret = "0"
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			reterr = r.(error)
+		}
+	}()
+	inta, reterr := toInt(num1)
+	if reterr != nil {
+		return
+	}
+	intb, reterr := toInt(num2)
+	if reterr != nil {
+		return
+	}
+	ret = strconv.FormatInt(inta/intb, 10)
+	return
 }
